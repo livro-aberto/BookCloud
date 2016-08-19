b34d61f7a6b34eb1e13ae0c5e82ef23467c1aac1
Augusto <augusto@impa.br>
2016-08-18 23:02:10 -0300

    Adding slot for comments

--- sphinx_edit/views.py
+++ sphinx_edit/views.py
@@ -7,6 +7,27 @@ import codecs # deals with encoding better
 #def index():
 #    return 'Hello World!'
 
+@app.route('/comments/<filename>')
+def get_comments(filename):
+    return "Comments from " + filename
+
+@app.route('/_static/<filename>')
+def resources(filename):
+    # to return a sub html
+    # with codecs.open('/tmp/bla/build/html/index.html', 'r', 'utf-8') as content_file:
+    #     content = content_file.read()
+    #
+    # content.encode('utf-8')
+    # print(content)
+    # return render_template('view.html', content=content)
+
+    #return render_template('view.html', content="abc")
+    #return '<iframe srcdoc="' + content + '</iframe>'
+    #return '<frame src="/tmp/bla/build/html/index.html"></iframe>'
+    return flask.send_from_directory( '/home/gutosurrex/gsync/Programming/grook/sphinx_edit/static/sphinx_static/',#app.config['UPLOAD_FOLDER'],
+        filename
+    )
+
 @app.route('/', defaults={'filename': 'index.html'})
 @app.route('/<path:filename>')
 def documentation(filename):
@@ -22,6 +43,6 @@ def documentation(filename):
     #return '<iframe srcdoc="' + content + '</iframe>'
     #return '<frame src="/tmp/bla/build/html/index.html"></iframe>'
     return flask.send_from_directory(
-        '/tmp/bla/build/html/',#app.config['UPLOAD_FOLDER'],
+        '/home/gutosurrex/rsync/Projetos/grook/bla/build/html/',#app.config['UPLOAD_FOLDER'],
         filename
     )
