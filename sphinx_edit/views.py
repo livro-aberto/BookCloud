c77f1e32ad1808f394233943ae7d5124939fbdc0
Augusto <augusto@impa.br>
2016-09-09 07:55:42 -0300

    Adding support for several projects

--- sphinx_edit/views.py
+++ sphinx_edit/views.py
@@ -1,7 +1,7 @@
 import os
 from os.path import join, isdir
 import flask
-from flask import render_template, render_template_string, request, redirect, url_for, Response
+from flask import render_template, render_template_string, request, redirect, url_for, Response, flash
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter, current_user
 from sphinx_edit import app
 
@@ -13,10 +13,26 @@ import sphinx
 
 config_path = 'conf'
 
-def check_main():
+# def check_main():
+#     # Check if the main repository has already been initiated. Otherwise, do it
+#     repo_path = 'repos/aaa/main'
+#     if (not os.path.isdir(repo_path)):
+#         os.makedirs(repo_path)
+#         git.Repo.init(repo_path)
+#         repo = git.Repo(repo_path)
+#         os.makedirs(join(repo_path, 'source'))
+#         copyfile('sphinx_edit/empty_repo/source/index.rst', join(repo_path, 'source/index.rst'))
+#         copyfile('sphinx_edit/empty_repo/.gitignore', join(repo_path, '.gitignore'))
+#         repo.index.add(['source/index.rst', '.gitignore'])
+#         repo.index.commit('Initial commit')
+#         build(join(repo_path, 'source'), join(repo_path, 'build/html'), config_path, '')
+
+def create_project(project_name, user_name):
     # Check if the main repository has already been initiated. Otherwise, do it
-    repo_path = 'repos/aaa/main'
-    if (not os.path.isdir(repo_path)):
+    repo_path = join('repos', project_name, user_name)
+    if os.path.isdir(repo_path):
+        return
+    else:
         os.makedirs(repo_path)
         git.Repo.init(repo_path)
         repo = git.Repo(repo_path)
@@ -57,39 +73,39 @@ def test():
 
     return render_template_string(content, standalone=True, render_sidebar=True)
 
-@app.route('/aaa/view/<path:filename>')
-def view(filename):
+@app.route('/<project>/view/<path:filename>')
+def view(project, filename):
     # Think if this should really be from the user
-    check_main()
+    # check_main()
     if (current_user.is_authenticated):
-        user_repo_path = join('repos/aaa', current_user.username)
+        user_repo_path = join('repos', project, current_user.username)
         check_user(current_user.username)
     else:
-        user_repo_path = join('repos/aaa', 'main')
+        user_repo_path = join('repos', project, 'main')
     filename, file_extension = os.path.splitext(filename)
     if file_extension == "":
         file_extension = '.html'
     with codecs.open(join(user_repo_path, 'build/html', filename + file_extension), 'r', 'utf-8') as content_file:
         content = content_file.read()
 
-    return render_template_string(content, content=content, reponame='aaa')
+    return render_template_string(content, content=content, reponame=project)
 
-@app.route('/aaa/save/<path:filename>', methods = ['GET', 'POST'])
+@app.route('/<project>/save/<path:filename>', methods = ['GET', 'POST'])
 @login_required
-def save(filename):
-    user_repo_path = join('repos/aaa', current_user.username)
+def save(project, filename):
+    user_repo_path = join('repos', project, current_user.username)
     if request.method == 'POST':
         with codecs.open(join(user_repo_path, 'source', filename + '.rst'), 'w') as dest_file:
             dest_file.write(request.form['code'].encode('utf8'))
     build(join(user_repo_path, 'source'), join(user_repo_path, 'build/html'), config_path, '')
     # return "Saved"
-    return redirect('/aaa/view/' + filename)
+    return redirect('/' + project + '/view/' + filename)
 
-@app.route('/aaa/edit/<path:filename>', methods = ['GET', 'POST'])
+@app.route('/<project>/edit/<path:filename>', methods = ['GET', 'POST'])
 @login_required
-def edit(filename):
+def edit(project, filename):
     filename, file_extension = os.path.splitext(filename)
-    user_repo_path = join('repos/aaa', current_user.username)
+    user_repo_path = join('repos', project, current_user.username)
     if request.method == 'POST':
         with codecs.open(join(user_repo_path, 'source', filename + '.rst'), 'w') as dest_file:
             dest_file.write(request.form['code'].encode('utf8'))
@@ -99,41 +115,65 @@ def edit(filename):
         rst = content_file.read()
     with codecs.open(join(user_repo_path, 'build/html', filename + '.html'), 'r', 'utf-8') as content_file:
         doc = render_template_string(content_file.read(), barebones=True)
-    return render_template('edit.html', doc=doc, rst=rst, filename=filename, reponame='aaa', render_sidebar=False)
+    return render_template('edit.html', doc=doc, rst=rst, filename=filename, reponame=project, render_sidebar=False)
 
-@app.route('/aaa/_images/<path:filename>', methods = ['GET'])
-@app.route('/edit/aaa/_images/<path:filename>', methods = ['GET'])
-def get_tikz(filename):
+@app.route('/<project>/_images/<path:filename>', methods = ['GET'])
+@app.route('/edit/<project>/_images/<path:filename>', methods = ['GET'])
+def get_tikz(project, filename):
     # Think if this should really be from the user
-    user_repo_path = join('repos/aaa', current_user.username)
+    user_repo_path = join('repos', project, current_user.username)
     return flask.send_from_directory(os.path.abspath(user_repo_path + '/build/html/_images/'), filename)
 
-@app.route('/comment_summary/aaa/<path:filename>')
-def comment_summary(filename):
+@app.route('/<project>/comment_summary/<path:filename>')
+def comment_summary(project, filename):
     return "Comments from " + filename
 
-@app.route('/aaa/<action>/_static/<path:filename>')
-def get_static(filename, action):
+@app.route('/<project>/<action>/_static/<path:filename>')
+def get_static(project, action, filename):
     if (current_user.is_authenticated):
-        user_repo_path = join('repos/aaa', current_user.username)
+        user_repo_path = join('repos', project, current_user.username)
     else:
-        user_repo_path = join('repos/aaa', 'main')
+        user_repo_path = join('repos', project, 'main')
     return flask.send_from_directory(os.path.abspath(join(user_repo_path, 'build/html/_static/')), filename)
 
 @app.route('/_static/<path:filename>')
 def get_global_static(filename):
     return flask.send_from_directory(os.path.abspath('conf/biz/static/'), filename)
 
-@app.route('/aaa')
-def index():
-    return redirect('aaa/index')
+@app.route('/<project>')
+def index(project):
+    return redirect(project + '/view/index')
 
 @app.route('/')
-def view_projects():
+def projects():
     path = 'repos'
     projects = [d for d in os.listdir(path) if isdir(join(path, d))]
-    bar_menu = [{'url': '/logout', 'name': 'logout'}]
+    if current_user.is_authenticated:
+        bar_menu = [{'url': '/logout', 'name': 'logout'},
+                    {'url': '/profile', 'name': current_user.username}]
+    else:
+        bar_menu = [{'url': '/login', 'name': 'login'}]
     return render_template('projects.html', projects=projects, bar_menu=bar_menu, copyright="CC-BY-SA-NC")
+
+@app.route('/profile')
+def profile():
+    if not current_user.is_authenticated:
+        redirect (url_for('login'))
+    bar_menu = [{'url': '/logout', 'name': 'logout'},
+                {'url': '/', 'name': 'home'}]
+    return render_template('profile.html', username=current_user.username, bar_menu=bar_menu)
+
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+    if not current_user.is_authenticated:
+        flash("You need to be logged in to create a new project")
+        return redirect (url_for('login'))
+    bar_menu = [{'url': '/logout', 'name': 'logout'},
+                {'url': '/', 'name': 'home'}]
+    if request.method == 'POST':
+        create_project(request.form['project'], current_user.username)
+        return redirect('/')
+    return render_template('new.html', username=current_user.username, bar_menu=bar_menu)
 
 @app.route('/_sources/<path:filename>')
 def show_source(filename):
@@ -143,13 +183,13 @@ def show_source(filename):
         content = content_file.read()
     return Response(content, mimetype='text/txt')
 
-@app.route('/aaa/images/<path:filename>')
-def get_image(filename):
-    return flask.send_from_directory(os.path.abspath('repos/aaa/images'), filename)
+@app.route('/<project>/images/<path:filename>')
+def get_image(project, filename):
+    return flask.send_from_directory(os.path.abspath('repos/' + project + '/images'), filename)
 
-@app.route('/aaa/view/genindex.html')
-def genindex():
-    return redirect('/aaa/view/index')
+@app.route('/<project>/view/genindex.html')
+def genindex(project):
+    return redirect('/' + project + '/view/index')
 
 @app.route('/login')
 def login():
