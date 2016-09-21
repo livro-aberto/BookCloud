0d597939ef5e5f1d460fbbba73504ddddf873597
Augusto <augusto@impa.br>
2016-09-21 13:20:30 -0300

    Started implementing database

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from flask_user import login_required, UserManager, UserMixin, SQLAlchemyAdapter, current_user
 
 
@@ -15,20 +16,37 @@ mail = Mail(app)                                # Initialize Flask-Mail
 # Define the User data model. Make sure to add flask.ext.user UserMixin !!!
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-
     # User authentication information
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, server_default='')
     reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
-
     # User email information
     # email = db.Column(db.String(255), nullable=False, unique=True)
     # confirmed_at = db.Column(db.DateTime())
-
     # User information
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
     first_name = db.Column(db.String(100), nullable=False, server_default='')
     last_name = db.Column(db.String(100), nullable=False, server_default='')
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    creator = relationship(User)
+
+    def __init__(self, name, creator):
+        self.name = name
+        self.creator = creator
+
+class Branch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    owner = relationship(User)
+    project = relationship(Project)
+
+    def __init__(self, name, owner, project):
+        self.name = name
+        self.owner = owner
+        self.project = project
 
 # Create all database tables
 db.create_all()
