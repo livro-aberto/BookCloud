a9107b9658e024c7f254518a7890071b6eb2061a
Augusto <augusto@impa.br>
2017-06-28 22:19:45 -0300

    Making separate config for instance

---
+++ instance/README.md
@@ -0,0 +1,8 @@
+Here you should keep the files that are specific to each instance.
+
+The most important example is the config file including variables such as
+* SECRET_KEY,
+* STRIPE_API_KEY,
+* SQLALCHEMY_DATABASE_URI
+
+
---
+++ instance/instance_config_default.py
@@ -0,0 +1,37 @@
+import os
+
+# Flask settings
+SECRET_KEY = 'THIS IS AN INSECURE SECRET'
+SQLALCHEMY_DATABASE_URI = ('mysql://user:pass'
+                           '@localhost/bookcloud')
+CSRF_ENABLED = True
+
+# Flask-Mail settings
+USER_ENABLE_EMAIL =          False
+USER_ENABLE_CONFIRM_EMAIL =  False
+USER_SEND_REGISTERED_EMAIL = False
+USER_ENABLE_LOGIN_WITHOUT_CONFIRM_EMAIL = False
+
+BOOKCLOUD_URL_PREFIX = '/'
+
+MAIL_USERNAME = 'email@example.com'
+MAIL_PASSWORD = 'password'
+MAIL_DEFAULT_SENDER = '"MyApp" <noreply@example.com>'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = int(True)
+
+ADMIN_MAIL = 'mail@example.com'
+
+# Flask-User settings
+USER_APP_NAME = "BookCloud"       # Used by email templates
+
+USER_PROPERTIES = (
+    { 'variable': 'integer_property_01',
+      'type': 'integer',
+      'title': 'State',
+      'choices': ('NY', 'NE', 'CA') },
+    { 'variable': 'boolean_property_01',
+      'type': 'boolean',
+      'title': 'Capital?' },
+)
