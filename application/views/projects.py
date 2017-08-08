0c69f7cd95edc7592f33eadf591ba82c5587a64d
Augusto <augusto@impa.br>
2017-08-08 14:02:56 -0300

    Fixing bug

--- application/views/projects.py
+++ application/views/projects.py
@@ -67,7 +67,6 @@ def project_context_processor():
 @projects.route('/dashboard', methods = ['GET', 'POST'])
 @limiter.exempt
 def dashboard(project):
-    a = treres
     tree = [ get_sub_branches(project.get_master()) ]
     log = project.get_master().get_log()
     master_path = project.get_master().get_source_path()
